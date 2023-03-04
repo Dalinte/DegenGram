9afccdd5f8fb0a3a5039b8895a97fedf29342caf
Dalinte <kollekcionerssd1@gmail.com>
2023-03-04 14:42:28 +0300

    add icons

--- src/app/providers/vuetify/index.ts
+++ src/app/providers/vuetify/index.ts
@@ -1,5 +1,18 @@
-// import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
-export const vuetify = createVuetify({})
+export const icons = {
+  defaultSet: 'mdi',
+  sets: {
+    mdi,
+  },
+  aliases,
+}
+
+export const vuetifyConfig = {
+  icons,
+}
+
+export const vuetify = createVuetify(vuetifyConfig)
