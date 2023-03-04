1ace28593ed00a9cef5a3279074adec6f49153d8
Dalinte <kollekcionerssd1@gmail.com>
2023-03-04 16:41:10 +0300

    add header

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/app/assets/logo.png"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <Layout>
+    <template v-slot:header>
+      <Header/>
+    </template>
+    <template v-slot:content>
+
+    </template>
+  </Layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from '@/features/HelloWorld.vue';
+import Layout from '@/shared/ui/layout/Layout.vue'
+import Header from '@/widgets/header/ui/Header.vue'
 
 export default defineComponent({
   name: 'HomePage',
   components: {
+    Header,
+    Layout,
     HelloWorld
   }
 })
---
+++ src/widgets/header/ui/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="d-flex justify-space-between">
+    <v-btn
+      class="ma-2"
+      color="indigo"
+      icon="mdi-baby-face-outline"
+    ></v-btn>
+    <v-btn>Connect wallet</v-btn>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header'
+})
+</script>
+
+<style scoped lang='scss'>
+
+</style>
