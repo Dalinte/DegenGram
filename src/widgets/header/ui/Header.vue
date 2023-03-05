2f32af40cddee2e0a8b9248fffb2bd02db89920f
Dalinte <kollekcionerssd1@gmail.com>
2023-03-05 14:35:04 +0300

    add layout

--- src/widgets/header/ui/Header.vue
+++ src/widgets/header/ui/Header.vue
@@ -1,22 +1,26 @@
 <template>
-  <header class="d-flex justify-space-between">
-    <v-btn
-      class="ma-2"
-      color="indigo"
-      icon="mdi-baby-face-outline"
-    ></v-btn>
-    <v-btn
-      v-if="!authStatus"
-      @click="toggleAuthStatus"
-    >
-      Connect wallet
-    </v-btn>
-    <UserLogout
-      v-if="authStatus"
-      address="0x690b9a9e9aa1c9db991c7721a92d351db4fac990"
-      @click="toggleAuthStatus"
-    />
-  </header>
+  <v-app-bar elevation="1">
+    <template v-slot:prepend>
+      <v-btn
+        class="ma-2"
+        color="indigo"
+        icon="mdi-baby-face-outline"
+      ></v-btn>
+    </template>
+    <template v-slot:append>
+      <v-btn
+        v-if="!authStatus"
+        @click="toggleAuthStatus"
+      >
+        Connect wallet
+      </v-btn>
+      <UserLogout
+        v-if="authStatus"
+        address="0x690b9a9e9aa1c9db991c7721a92d351db4fac990"
+        @click="toggleAuthStatus"
+      />
+    </template>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
