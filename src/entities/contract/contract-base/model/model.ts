686456cd9166b6627c41f8e0e590562a7d9ea6ff
Итигечев Роман <itigichev_roman@smartworld.team>
2023-03-26 16:14:02 +0700

    add contracts method

--- src/entities/contract/contract-base/model/model.ts
+++ src/entities/contract/contract-base/model/model.ts
@@ -1,6 +1,7 @@
 import { Contract as ContractInterface } from 'web3-eth-contract'
 import { AbiItem } from 'web3-utils'
 
+// eslint-disable-next-line import/no-unresolved
 import { Web3 } from '@/shared/lib'
 
 export class Contract {
